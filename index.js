ba8ed234ad09e71ce2d4e81b6ea73bf79d30f42c
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-04-06 00:49:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Declare customerName to be bob in global scope
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  // Modify the customerName variable
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // Set bestCustomer to 'not bob'
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+  // Overwrite the best customer
+  window.bestCustomer = newCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // Unsuccessfully try to reassign the least favorite customer
+  leastFavoriteCustomer = 'someone else';
+}
